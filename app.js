91208897dfccdf21d28805fdbb7e16196ce1c412
Jere Stigell <jj.stigell@gmail.com>
2022-08-16 13:43:41 +0900

    11.6 Fix the code (lint errors)

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const express = require("express");
-const app = express();
-const port = 5001;
+const express = require('express')
+const app = express()
+const port = 5001
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || port;
+const PORT = process.env.PORT || port
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${port}`);
-});
+  // eslint-disable-next-line no-console
+  console.log(`server started on port ${port}`)
+})
